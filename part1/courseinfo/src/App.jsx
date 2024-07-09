00d52a34c655bfe81dc907efc50bc060921b5145
Atlegang Sethono <atlegangcode@gmail.com>
2024-07-09 10:31:17 +0200

    Complete exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,15 +1,9 @@
-import { Header } from "./Header"
-import { Content } from "./Content"
-import { Total } from "./Total"
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { Total } from "./Total";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
 
   return (
     <div>
@@ -17,7 +11,7 @@ const App = () => {
       <Content />
       <Total />
     </div>
-  )
-}
+  );
+};
 
-export { App }
+export { App };
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -2,10 +2,8 @@ const Total = () => {
   const exercises1 = 10;
   const exercises2 = 7;
   const exercises3 = 14;
-    const total = exercises1 + exercises2 + exercises3
-    return (
-        <p>Number of exercises {total}</p>
-    )
-}
+  const total = exercises1 + exercises2 + exercises3;
+  return <p>Number of exercises {total}</p>;
+};
 
-export { Total }
+export { Total };
