d6a3e55a3c7f68a6b0642a14b26db442a78daf5f
Atlegang Sethono <atlegangcode@gmail.com>
2024-09-16 09:12:43 +0200

    Reset App.jsx - add boiler/starter code

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,151 +1,24 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import "./index.css";
-import { Filter } from "./components/Filter";
-import { PersonForm } from "./components/PersonForm";
-import { Persons } from "./components/Persons";
-import personService from "./services/persons";
-import { Notification } from "./components/Notification";
+import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [searchPerson, setSearchPerson] = useState("");
-  const [filteredPerson, setFilteredPerson] = useState([]);
-  const [successMessage, setSuccessMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
-  useEffect(() => {
-    personService.getAll().then((initialPersons) => {
-      setPersons(initialPersons);
-      setFilteredPerson(initialPersons);
-    });
-  }, []);
-
-  const appendName = (event) => {
-    event.preventDefault();
-    console.log(event.target);
-
-    const checkName = persons.find(
-      (person) => person.name.toLowerCase() === newName.toLowerCase()
-    );
-
-    const nameObj = {
-      name: newName,
-      id: (persons.length + 1).toString(),
-      number: newNumber,
-    };
-
-    if (checkName) {
-      const confirmed = window.confirm(
-        `${checkName.name} is already added to the phone book. Update number?`
-      );
-      if (!confirmed) {
-        return;
-      }
-      personService.update(checkName.id, nameObj).then((updatedPerson) => {
-        setPersons((previousPerson) => {
-          previousPerson.id === checkName.id ? updatedPerson : persons;
-        });
-        setFilteredPerson((previousFilteredPerson) => {
-          previousFilteredPerson.id === checkName.id ? updatedPerson : persons;
-        });
-        setSuccessMessage(`${updatedPerson.name} has been updated successfully`);
-        setTimeout(() => {
-          setSuccessMessage("");
-        }, 3000);
-      })
-      .catch((error) => {
-        console.log("Error updating the number:", error.message);
-        alert("Error updating the number");
-        setErrorMessage(`${nameExists.name} has already been removed`)
-        setTimeout(() => {
-          setErrorMessage("");
-        }, 3000);
-      });
-    } else {
-      personService
-        .create(nameObj)
-        .then((returnedPerson) => {
-          setPersons(persons.concat(returnedPerson));
-          setFilteredPerson(filteredPerson.concat(returnedPerson));
-          setSuccessMessage(`Added ${returnedPerson.name} to Phone Book`);
-          setTimeout(() => {
-            setSuccessMessage("");
-          }, 3000);
-        })
-        .catch((error) => {
-          console.log("Error updating the number:", error.message);
-          alert("Error updating the number");
-        });
-    }
-    setNewName("");
-    setNewNumber("");
-  };
-
-  const deletePersonEntry = (id, name) => {
-    const confirmDelete = window.confirm(`Delete ${name}?`);
-    if (!confirmDelete) {
-      return;
-    }
-
-    personService
-      .deleteRequest(id)
-      .then(() => {
-        setPersons(persons.filter((person) => person.id !== id));
-        setFilteredPerson(filteredPerson.filter((person) => person.id !== id));
-      })
-      .catch((error) => {
-        console.log("Error deleting person, ", error.message);
-        alert("Error deleting person");
-      });
-  };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-    console.log(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-    console.log(event.target.value);
-  };
-
-  const handleSearch = (event) => {
-    setSearchPerson(event.target.value);
-    console.log(event.target.value);
-
-    const filterName = persons.filter((person) =>
-      person.name.toLowerCase().includes(event.target.value.toLowerCase())
-    );
-    setFilteredPerson(filterName);
-  };
+  const [person, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
 
   return (
     <div>
-      <h2>Phone Book</h2>
-      {/* <Notification successMessage={successMessage} /> */}
-      {/* {successMessage} */}
-      <Notification message={successMessage} isError={false} />
-      <Notification message={errorMessage} isError={false} />
-      <Filter searchPerson={searchPerson} handleSearch={handleSearch} />
-      <h3>Add a new</h3>
-      <PersonForm
-        appendName={appendName}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-        // successMessage={successMessage}
-      />
-      <h3>Numbers</h3>
-      <Persons
-        filteredPerson={filteredPerson}
-        deletePersonEntry={deletePersonEntry}
-      />
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
     </div>
-  );
-};
+  )
+}
 
-export { App };
+export { App }
