1952475e5c16d4c53a6af46df3adb37383a31cfb
Atlegang Sethono <atlegangcode@gmail.com>
2024-09-16 10:51:01 +0200

    Implement loguc that adds a person to the phone book

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
-import { useState } from 'react'
-import { Names } from './Names';
+import React from "react";
+import { useState } from "react";
+import { Names } from "./Names";
 
 const App = (props) => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setPersons([...persons, {name: newName}]);
+    setNewName('');
+
+  }
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
+      <h2>Phone book</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-        {persons.map((person) => {
-           return <Names key={person.id} person={person} />
-        })}
+      {persons.map((person) => {
+        return <Names person={person} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export { App }
+export { App };
