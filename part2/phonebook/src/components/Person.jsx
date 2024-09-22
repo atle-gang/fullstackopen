1e86cbb12bd13fb5df2e53b07bf7e7376272edb4
Atlegang Sethono <atlegangcode@gmail.com>
2024-09-22 11:02:59 +0200

    Add delete button to Person component for removing entries

--- part2/phonebook/src/components/Person.jsx
+++ part2/phonebook/src/components/Person.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Person = ({ person }) => {
+const Person = ({ person, deletePersonEntry }) => {
   return (
     <div>
       {person.name} - {person.number}
+      <button onClick={() => deletePersonEntry(person.id, person.name)}>delete</button>
     </div>
   );
 };
