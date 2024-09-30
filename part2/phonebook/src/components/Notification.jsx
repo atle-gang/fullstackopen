160fd4bf40693cdd6a087c637517bf0173005802
Atlegang Sethono <atlegangcode@gmail.com>
2024-09-30 12:42:29 +0200

    Refactor Notification component to use props the differentiate Notifcation types and messages

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Notification = ({ message }) => {
-  if (message == null) {
+const Notification = (props) => {
+  if (props.notification == null) {
     return null;
   }
 
-  return <div className="success">{message}</div>;
+  return <div className={props.notification.type}>{props.notification.message}</div>;
 };
 
 export { Notification };
