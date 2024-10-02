d6721284ba78dad348db257cca0a46cffea02519
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-02 12:16:50 +0200

    Refactor getCountries function to allow the handling of errors if they occur

--- part2/countries/src/services/countriesService.js
+++ part2/countries/src/services/countriesService.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
 
 const getCountries = () => {
-    const request = axios.get(baseUrl);
-    return request.then((response) => response.data)
-}
+  return axios
+    .get(baseUrl)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error fetching countries: ", error.message);
+      return [];
+    });
+};
 
 export { getCountries };
