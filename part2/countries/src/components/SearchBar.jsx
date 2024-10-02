34ad7b57c49bcfc9bc3edb77a3ded5efb4e85747
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-02 11:38:39 +0200

    Add SearchBar component that will filter countrie based on user input

---
+++ part2/countries/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchBar = ({ searchQuery, handleSearchQuery }) => {
+  return (
+    <div>
+      find countries:{" "}
+      <input value={searchQuery} type="text" onChange={handleSearchQuery} />
+    </div>
+  )
+}
+
+export { SearchBar };
