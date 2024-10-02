a6410d43822cb3e39231865038515f6d8b987207
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-02 12:24:27 +0200

    Optimise handleSearchQuery function by checking if new state is equal to previous state so component does not render

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import { useState } from 'react'
-import { SearchBar } from './components/SearchBar';
+import React from "react";
+import { useState } from "react";
+import { SearchBar } from "./components/SearchBar";
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearchQuery = (event) => {
-    setSearchQuery(event.target.value);
-  }
+    const query = event.target.value.trim();
+
+    // Optimising state updates by checking if the new search query is different from the current state.
+    // This prevents unnecessary re-renders and improves performance, especially when the input hasn't changed meaningfully.
+    if (query === searchQuery) return;
+
+    setSearchQuery(query);
+  };
 
   return (
     <div>
-      <SearchBar searchQuery={searchQuery} handleSearchQuery={handleSearchQuery} />
+      <SearchBar
+        searchQuery={searchQuery}
+        handleSearchQuery={handleSearchQuery}
+      />
     </div>
-  )
-}
+  );
+};
 
 export { App };
