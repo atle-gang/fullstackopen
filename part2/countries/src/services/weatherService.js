050340a2f08e4ab6b216f3dda8715691089ba453
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-04 14:41:59 +0200

    Include 'VITE' keyword in env and weatherService files

--- part2/countries/src/services/weatherService.js
+++ part2/countries/src/services/weatherService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchWeather = async (capitalCity) => {
-  const apiKey = import.meta.env.API_KEY;
+  const apiKey = import.meta.env.VITE_API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${capitalCity}&appid=${apiKey}&units=metric`;
   
   try {
