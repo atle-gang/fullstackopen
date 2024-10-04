71107bc39fad6e3cd6db4142088990c1b66b9937
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-04 13:58:32 +0200

    Create weatherService module that will be responsible for fetching data for city

--- part2/countries/src/services/weatherService.js
+++ part2/countries/src/services/weatherService.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
-const fetchWeather = () => {
-
-}
+const fetchWeather = async (capitalCity) => {
+  const apiKey = process.env.API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${capitalCity}&appid=${apiKey}&units=metric`;
+  console.log('API Key:', process.env.API_KEY); // Check if it logs the actual key
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export default { fetchWeather };
+
