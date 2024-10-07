e6037575507039ad9ee5a7bcb1ec92be31ab9b4e
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-07 18:02:20 +0200

    Push code - phone book not fetching data locally

--- part2/phonebook/src/services/personsService.js
+++ part2/phonebook/src/services/personsService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://fso-phonebook-backend-4gi7.onrender.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
