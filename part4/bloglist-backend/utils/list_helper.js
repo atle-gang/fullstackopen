5c0f64928c49b51b08b1d7063d9a37251d32391f
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-10 14:57:33 +0200

    Create totalLikes function and tests that validates it

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (listOfBlogs) => {
+  const reducer = (total, currentItem) => {
+    return total + currentItem.likes;
+  }
+
+  return listOfBlogs.reduce(reducer, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
