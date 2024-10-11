1fa3472e702111a5279c63422a5cdac5a6b1a3bb
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-11 16:37:04 +0200

    Refactor tests by grouping and organising them for readability

--- part4/bloglist-backend/tests/blog_api.test.js
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -1,5 +1,5 @@
-const { test, after, beforeEach } = require("node:test");
-const { only } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
+const { } = require("node:test");
 const mongoose = require("mongoose");
 const assert = require("node:assert");
 const supertest = require("supertest");
@@ -17,79 +17,83 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test.only("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+describe("when there is initially some notes saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test.only("blog list returns correct amount of blog posts", async () => {
-  const response = await api.get("/api/blogs");
+  test("blog list returns correct amount of blog posts", async () => {
+    const response = await api.get("/api/blogs");
 
-  assert.strictEqual(response.body.length, testHelper.initialBlogList.length);
-});
+    assert.strictEqual(response.body.length, testHelper.initialBlogList.length);
+  });
 
-test.only("verifies that the unique identifier properly of the blog posts is named id", async () => {
-  const response = await api.get("/api/blogs");
+  test("verifies that the unique identifier properly of the blog posts is named id", async () => {
+    const response = await api.get("/api/blogs");
 
-  response.body.forEach((blog) => {
-    assert.ok(blog.id);
+    response.body.forEach((blog) => {
+      assert.ok(blog.id);
+    });
   });
 });
 
-test.only("verify that making an HTTP POST request successfully creates a new blog post", async () => {
-  const testBlog = {
-    title: "Test Blog",
-    author: "Test Author",
-    url: "test/url",
-    likes: 0
-  };
+describe("addition of a new blog", () => {
+  test("verify that making an HTTP POST request successfully creates a new blog post", async () => {
+    const testBlog = {
+      title: "Test Blog",
+      author: "Test Author",
+      url: "test/url",
+      likes: 0
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(testBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/blogs")
+      .send(testBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  blogsAtEnd = await testHelper.blogsInDB();
-  assert.strictEqual(blogsAtEnd.length, testHelper.initialBlogList.length + 1);
+    blogsAtEnd = await testHelper.blogsInDB();
+    assert.strictEqual(blogsAtEnd.length, testHelper.initialBlogList.length + 1);
 
-  const response = await api.get("/api/blogs");
-  const titles = response.body.map(blog => blog.title);
-  assert(titles.includes("Test Blog"));
-});
+    const response = await api.get("/api/blogs");
+    const titles = response.body.map(blog => blog.title);
+    assert(titles.includes("Test Blog"));
+  });
 
-test.only("likes property gets added if not included in request body", async () => {
-  const blogWithNoLikes = {
-    title: "Test With no Likes",
-    author: "Test No Likes",
-    url: "test/no/likes"
-  };
+  test("likes property gets added if not included in request body", async () => {
+    const blogWithNoLikes = {
+      title: "Test With no Likes",
+      author: "Test No Likes",
+      url: "test/no/likes"
+    };
 
-  const response = await api
-    .post("/api/blogs")
-    .send(blogWithNoLikes)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    const response = await api
+      .post("/api/blogs")
+      .send(blogWithNoLikes)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  assert.strictEqual(response.body.likes, 0);
-});
+    assert.strictEqual(response.body.likes, 0);
+  });
 
-test.only("a valid blog can be added", async () => {
-  const invalidBlog = {
-    author: "Test No Likes",
-    likes: 5
-  };
+  test("a valid blog can be added", async () => {
+    const invalidBlog = {
+      author: "Test No Likes",
+      likes: 5
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(invalidBlog)
-    .expect(400);
+    await api
+      .post("/api/blogs")
+      .send(invalidBlog)
+      .expect(400);
 
-  const notesAtEnd = await testHelper.blogsInDB();
+    const notesAtEnd = await testHelper.blogsInDB();
 
-  assert.strictEqual(notesAtEnd.length, testHelper.initialBlogList.length);
+    assert.strictEqual(notesAtEnd.length, testHelper.initialBlogList.length);
+  });
 });
 
 after(async () => {
