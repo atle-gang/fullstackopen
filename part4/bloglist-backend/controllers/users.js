e522a513a34dd3f99cbbad317b4c344b8e013db0
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-14 16:27:38 +0200

    Implement a get method to see the details of all users by doing a HTTP GET request

--- part4/bloglist-backend/controllers/users.js
+++ part4/bloglist-backend/controllers/users.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
 const User = require('../models/user');
 
+usersRouter.get("/", async (request, response, next) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
 usersRouter.post("/", async (request, response, next) => {
   try {
     const { name, username, password } = request.body;
