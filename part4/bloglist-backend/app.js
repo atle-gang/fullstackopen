66b3de5350bf30763a42997295483f1a56b8a06a
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-14 17:17:02 +0200

    Create unknownEndpoints and errorHandler functions and implement them in the app.js file

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -3,7 +3,7 @@ const usersRouter = require('./controllers/users');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
-const morganMiddleware = require('./utils/middleware');
+const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const express = require('express');
 const app = express();
@@ -22,9 +22,12 @@ mongoose.connect(mongoUrl)
 
 app.use(cors());
 app.use(express.json());
-app.use(morganMiddleware);
+app.use(middleware.morganMiddleware);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app; 
--- part4/bloglist-backend/utils/middleware.js
+++ part4/bloglist-backend/utils/middleware.js
@@ -1,9 +1,29 @@
 const morgan = require("morgan");
 
+const unknownEndpoint = (request, response, next) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+    return response.status(400).json({ error: 'expected `username` to be unique' })
+  }
+
+  next(error)
+}
+
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
 
-const morganMiddleware = morgan(':method :url :status :res[content-length] - :response-time ms :body')
+const morganMiddleware = morgan(':method :url :status :res[content-length] - :response-time ms :body');
 
-module.exports = morganMiddleware;
+module.exports = {
+  morganMiddleware, 
+  unknownEndpoint,
+  errorHandler 
+};
