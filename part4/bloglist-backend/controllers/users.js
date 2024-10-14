d57778f233d5981ebf7011d2e1ebeb625c17a43d
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-14 16:51:54 +0200

    Implement HTTP DELETE method

--- part4/bloglist-backend/controllers/users.js
+++ part4/bloglist-backend/controllers/users.js
@@ -46,6 +46,15 @@ usersRouter.post("/", async (request, response, next) => {
   }
 });
 
+usersRouter.delete("/:id", async (request, response) => {
+  try {
+    await User.findByIdAndDelete(request.params.id);
+    response.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = usersRouter;
 
