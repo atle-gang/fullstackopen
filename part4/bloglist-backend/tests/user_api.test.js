475dea98ca8296f2a1e20590df6ba979cdd0c856
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-28 11:28:23 +0200

    Write test that tests when username has been taken

--- part4/bloglist-backend/tests/user_api.test.js
+++ part4/bloglist-backend/tests/user_api.test.js
@@ -12,31 +12,52 @@ const bcrypt = require('bcrypt');
 let dwightToken;
 let michaelToken;
 
-beforeEach(async() => {
-    await Blog.deleteMany({});
-    await User.deleteMany({});
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await User.deleteMany({});
 
-    // Create users
-    await api.post("api/users").send(testHelper.testUsers[0]);
-    await api.post("api/users").send(testHelper.testUsers[1]);
+  // Create users
+  await api.post("/api/users").send(testHelper.testUsers[0]);
+  await api.post("/api/users").send(testHelper.testUsers[1]);
 
-    // Get tokens
-    const dwightLogin = await api.post("/api/login").send(helper.testUsers[0]);
-    const michaelLogin = await api.post("/api/login").send(helper.testUsers[1]);
-    dwightToken = `bearer ${dwightLogin.body.token}`;
-    michaelToken = `bearer ${michaelLogin.body.token}`;
+  // Get tokens
+  const dwightLogin = await api.post("/api/login").send(testHelper.testUsers[0]);
+  const michaelLogin = await api.post("/api/login").send(testHelper.testUsers[1]);
+  dwightToken = `bearer ${dwightLogin.body.token}`;
+  michaelToken = `bearer ${michaelLogin.body.token}`;
 
-    // Create blogs for users
-    const dwightBlogs = testHelper.dwightBlogs;
-    await api.post("/api/blogs").send(testHelper.dwightBlogs[0]).set("Authorization", dwightToken);
-    await api.post("/api/blogs").send(testHelper.dwightBlogs[1]).set("Authorization", dwightToken);
-    await api.post("/api/blogs").send(testHelper.dwightBlogs[2]).set("Authorization", dwightToken);
+  // Create blogs for users
+  const dwightBlogs = testHelper.dwightBlogs;
+  await api.post("/api/blogs").send(testHelper.dwightBlogs[0]).set("Authorization", dwightToken);
+  await api.post("/api/blogs").send(testHelper.dwightBlogs[1]).set("Authorization", dwightToken);
+  await api.post("/api/blogs").send(testHelper.dwightBlogs[2]).set("Authorization", dwightToken);
 
-    const michaelBlogs = testHelper.michaelBlogs;
-    await api.post("/api/blogs").send(testHelper.michaelBlogs[0]).set("Authorization", michaelToken);
-    await api.post("/api/blogs").send(testHelper.michaelBlogs[1]).set("Authorization", michaelToken);
-    await api.post("/api/blogs").send(testHelper.michaelBlogs[2]).set("Authorization", michaelToken);
-})
+  const michaelBlogs = testHelper.michaelBlogs;
+  await api.post("/api/blogs").send(testHelper.michaelBlogs[0]).set("Authorization", michaelToken);
+  await api.post("/api/blogs").send(testHelper.michaelBlogs[1]).set("Authorization", michaelToken);
+  await api.post("/api/blogs").send(testHelper.michaelBlogs[2]).set("Authorization", michaelToken);
+});
+
+describe("Invalid request to users", () => {
+  test("test fails if username is taken", async () => {
+    const usersAtStart = await testHelper.usersInDB();
+
+    const newUser = {
+      username: testHelper.testUsers[0].username,
+      name: "Mark",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    assert.strictEqual(result.body.error, /expected `username` to be unique/);
+    const usersAtEnd = await testHelper.usersInDB();
+    assert.strictEqual(usersAtStart.length, usersAtEnd.length);
+  });
+});
 
 
 after(async () => {
