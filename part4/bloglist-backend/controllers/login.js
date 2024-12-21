f549d8ce8251f8a325b679c5330395ae924830bf
Atlegang Sethono <atlegangcode@gmail.com>
2024-12-21 16:37:49 +0200

    Delete hanging comment

--- part4/bloglist-backend/controllers/login.js
+++ part4/bloglist-backend/controllers/login.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const loginRouter = require('express').Router();
 const User = require('../models/user');
-// require("dotenv").config();
 
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body;
