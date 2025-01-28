68ebce8284fb061d448a6bf3491974c212f88641
Atlegang Sethono <atlegangcode@gmail.com>
2025-01-28 17:20:22 +0200

    Write test for successful login

--- part5/playwright-tests/tests/noteApp.spec.js
+++ part5/playwright-tests/tests/noteApp.spec.js
@@ -5,10 +5,21 @@ describe("Blog app", () => {
     await page.goto("http://localhost:5173");
   });
 
-  test("Login form is shown", async ({ page }) => {
-    const headingLocator = page.getByRole("heading", { name: "Login" });
-    const buttonLocator = page.getByRole("button", { name: "login" });
-    await expect(headingLocator).toBeVisible();
-    await expect(buttonLocator).toBeVisible();
+    test("Login form is shown", async ({ page }) => {
+      const headingLocator = page.getByRole("heading", { name: "Login" });
+      const buttonLocator = page.getByRole("button", { name: "login" });
+      await expect(headingLocator).toBeVisible();
+      await expect(buttonLocator).toBeVisible();
+    });
+
+  test("User can successfully log in", async ({ page }) => {
+    await page.getByTestId("username").fill("test-user");
+    await page.getByTestId("password").fill("test123");
+    await page.getByRole("button", { name: "login" }).click();
+
+    const blogHeader = page.locator('[data-testid="blogs-header"]');
+    const userHasLoggedIn = page.locator('[data-testid="user-has-logged-in"]');
+    await expect(blogHeader).toBeVisible();
+    await expect(userHasLoggedIn).toBeVisible();
   });
 });
