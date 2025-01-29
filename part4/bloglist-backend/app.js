2eda441320cf64aafbdfedff7af25c782c4411f5
Atlegang Sethono <atlegangcode@gmail.com>
2025-01-29 17:12:11 +0200

    Integrate testing route in 'app.js'

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -1,20 +1,21 @@
-const blogsRouter = require('./controllers/blogs');
-const usersRouter = require('./controllers/users');
-const loginRouter = require('./controllers/login');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const middleware = require('./utils/middleware');
-const logger = require('./utils/logger');
-const express = require('express');
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const express = require("express");
 const app = express();
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 const mongoUrl = config.MONGODB_URI;
 
-mongoose.connect(mongoUrl)
-  .then(result => {
+mongoose
+  .connect(mongoUrl)
+  .then((result) => {
     logger.info("Connected to MongoDB");
   })
   .catch((error) => {
@@ -26,11 +27,16 @@ app.use(express.json());
 app.use(middleware.morganMiddleware);
 app.use(middleware.tokenExtractor);
 
-app.use('/api/blogs', blogsRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/login', loginRouter);
+app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
-module.exports = app; 
+module.exports = app;
