74701be458cb137438e22094ff45d54a3595020c
Atlegang Sethono <atlegangcode@gmail.com>
2025-01-31 13:31:30 +0200

    Add test-identifier to Togglable component

--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -14,7 +14,7 @@ const Togglable = (props) => {
   return (
     <div>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+        <button onClick={toggleVisibility} data-testid="toggle-button">{props.buttonLabel}</button>
       </div>
       <div style={showWhenVisible}>
         {props.children}
