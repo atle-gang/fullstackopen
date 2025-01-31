e1af25c0765aa5f5b92b90f5a5038d4819f85051
Atlegang Sethono <atlegangcode@gmail.com>
2025-01-31 13:49:39 +0200

    Create test for creating a blog

--- part5/playwright-tests/tests/noteApp.spec.js
+++ part5/playwright-tests/tests/noteApp.spec.js
@@ -21,25 +21,61 @@ describe("Blog app", () => {
     await expect(buttonLocator).toBeVisible();
   });
 
-  test("Allows user to log in successfully", async ({ page }) => {
-    await page.getByTestId("username").fill("test-user");
-    await page.getByTestId("password").fill("test123");
-    await page.getByRole("button", { name: "login" }).click();
+  describe("Login", () => {
+    test("Allows user to log in successfully", async ({ page }) => {
+      await page.getByTestId("username").fill("test-user");
+      await page.getByTestId("password").fill("test123");
+      await page.getByRole("button", { name: "login" }).click();
 
-    const blogHeader = page.locator('[data-testid="blogs-header"]');
-    const userHasLoggedIn = page.locator('[data-testid="user-has-logged-in"]');
-    await expect(blogHeader).toBeVisible();
-    await expect(userHasLoggedIn).toBeVisible();
+      const blogHeader = page.locator('[data-testid="blogs-header"]');
+      const userHasLoggedIn = page.locator(
+        '[data-testid="user-has-logged-in"]'
+      );
+      await expect(blogHeader).toBeVisible();
+      await expect(userHasLoggedIn).toBeVisible();
+    });
+
+    // TODO: Improve test for notification visibility
+    // Had a hard time testing the Notification component that outputs a message when login is unsuccessful
+    test("User fails to log in", async ({ page }) => {
+      await page.getByTestId("username").fill("test-user");
+      await page.getByTestId("password").fill("123test");
+      await page.getByRole("button", { name: "login" }).click();
+
+      const loginHeader = page.locator('[data-testid="login-header"]');
+      await expect(loginHeader).toBeVisible();
+    });
   });
 
-  // TODO: Improve test for notification visibility
-  // Had a hard time testing the Notification component that outputs a message when login is unsuccessful
-  test("User fails to log in", async ({ page }) => {
-    await page.getByTestId("username").fill("test-user");
-    await page.getByTestId("password").fill("123test");
-    await page.getByRole("button", { name: "login" }).click();
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      await page.getByTestId("username").fill("test-user");
+      await page.getByTestId("password").fill("test123");
+      await page.getByRole("button", { name: "login" }).click();
+    });
 
-    const loginHeader = page.locator('[data-testid="login-header"]');
-    await expect(loginHeader).toBeVisible();
+    test("User creates a new blog", async ({ page }) => {
+      // User logs in
+      await expect(page.getByText("Test User logged in ")).toBeVisible()
+      await expect(page.getByText("No blogs found. Create your first blog")).toBeVisible();
+      const newBlogBtn = page.getByTestId("toggle-button")
+      await expect(newBlogBtn).toBeVisible()
+      // Check to see if "new button" is visible
+      await page.getByRole("button", { name: "new blog"}).click()
+
+      // Check to see if form header gets rendered after previous click
+      await expect(page.getByText("Create new blog")).toBeVisible()
+
+      // User fills form in
+      await page.getByTestId("title").fill("Title Test");
+      await page.getByTestId("author").fill("Author Test");
+      await page.getByTestId("url").fill("URL Test");
+
+      // Click 'create' button
+      await page.getByRole("button", { name: "create" }).click();
+      // Check to see if blog data is displayed
+      const blogList = page.locator('[data-testid="blog-list"]')
+      expect(blogList.getByText);
+    });
   });
 });
