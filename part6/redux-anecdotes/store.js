186242d749ce893823c126a6a9995c98c49fa0cf
Atlegang Sethono <atlegangcode@gmail.com>
2025-02-10 12:53:10 +0200

    Create notificationReducer

--- part6/redux-anecdotes/store.js
+++ part6/redux-anecdotes/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  filterReducer  from "./src/reducers/filterReducer";
 import  anecdoteReducer  from "./src/reducers/anecdoteReducer";
+import notificationReducer from "./src/reducers/notificationReducer"
 
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
     filter: filterReducer,
+    notification: notificationReducer
   },
 });
 
