07b1f65c35a5e2c0acc369e36312c7c8eb1512ff
Atlegang Sethono <atlegangcode@gmail.com>
2025-03-10 10:03:34 +0200

    Enhance `notification slice with timed auto-clear functionality
    - Introduce `setNotification` thunk to dispatch and auto-clear notifications after a delay
    - Use action renaming `setNotificationAction` to avoid conflicts
    - Add a timeout mechanism to prevent overlapping notifications

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -12,7 +12,25 @@ const notificationSlice = createSlice({
     },
   },
 });
-
-export const { setNotification, clearNotification } =
+// setNotification: setNotificationAction (Destructuring with renaming)
+const { setNotification: setNotificationAction, clearNotification } =
   notificationSlice.actions;
+
+let timeoutId = null;
+
+export const setNotification = (message, seconds = 3) => {
+  return async (dispatch) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    dispatch(setNotificationAction(message));
+
+    timeoutId = setTimeout(() => {
+      dispatch(clearNotification());
+      timeoutId = null;
+    }, seconds * 1000);
+  };
+};
+
 export default notificationSlice.reducer;
