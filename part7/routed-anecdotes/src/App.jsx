02c066894fad78e37c7e390fdfe307127fe3b5fd
Atlegang Sethono <atlegangcode@gmail.com>
2025-03-14 17:54:10 +0200

    Wrap application in `<Router>` and define routes using `<Routes>`

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import { About } from "./components/About";
 import { Menu } from "./components/Menu";
 import { AnecdoteList } from "./components/AnecdoteList";
 import { Footer } from "./components/Footer";
-import { CreateNew  } from "./components/CreateNew";
+import { CreateNew } from "./components/CreateNew";
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
@@ -45,14 +47,18 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <h1>Software anecdotes</h1>
+        <Menu />
+        <Routes>
+          <Route path="/create-new" element={<CreateNew addNew={addNew} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
